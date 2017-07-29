15800483155b45bc13b15e108fd638cd47a58f8e
Paul Traylor <kungfudiscomonkey@gmail.com>
2017-07-29 18:20:51 +0900

    Some basic setup

--- nagger/nagger/AppDelegate.swift
+++ nagger/nagger/AppDelegate.swift
@@ -10,17 +10,24 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
 
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.title = "WeatherBar"
+        statusItem.menu = statusMenu
+
+        let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-

08b394cc517e059735c89feeec23ab88f8e1f7e5
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-08-19 17:21:19 +0900

    Refactor out some duplicate code

--- NextPomodoro/PomodoroAPI.swift
+++ NextPomodoro/PomodoroAPI.swift
@@ -67,9 +67,8 @@ struct PomodoroResponse : Codable {
     let results: [Pomodoro]
 }
 
-func checkLogin(username: String, password: String, completionHandler: @escaping (HTTPURLResponse) -> Void) {
-    let url = URL.init(string: ApplicationSettings.pomodoroAPI)
-    var request = URLRequest.init(url: url!)
+func authRequest(username: String, password: String, url: String, completionHandler: @escaping (HTTPURLResponse, Data) -> Void) {
+    var request = URLRequest.init(url: URL.init(string: url)!)
 
     let loginString = "\(username):\(password)"
 
@@ -85,78 +84,45 @@ func checkLogin(username: String, password: String, completionHandler: @escaping
 
     let task = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error -> Void in
         if let httpResponse = response as? HTTPURLResponse {
-            completionHandler(httpResponse)
+            completionHandler(httpResponse, data!)
         }
     })
 
     task.resume()
 }
 
-func getFavorites(completionHandler: @escaping ([Favorite]) -> Void) {
-    let url = URL.init(string: ApplicationSettings.favoriteAPI)
-    var request = URLRequest.init(url: url!)
-
-    let loginString = "\(ApplicationSettings.username!):\(ApplicationSettings.password!)"
-
-    guard let loginData = loginString.data(using: String.Encoding.utf8) else {
-        return
-    }
-    let base64LoginString = loginData.base64EncodedString()
-
-    request.httpMethod = "GET"
-    request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("application/json", forHTTPHeaderField: "Accept")
-
-    let task = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error -> Void in
-        if let httpResponse = response as? HTTPURLResponse {
-            print(httpResponse)
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                do {
-                    let favorites = try decoder.decode(FavoriteResponse.self, from: data!)
-                    completionHandler(favorites.results)
-                } catch let error {
-                    print(error)
-                }
-            }}
+func checkLogin(username: String, password: String, completionHandler: @escaping (HTTPURLResponse) -> Void) {
+    authRequest(username: username, password: password, url: ApplicationSettings.pomodoroAPI, completionHandler: {response, data in
+        completionHandler(response)
     })
+}
 
-    task.resume()
+func getFavorites(completionHandler: @escaping ([Favorite]) -> Void) {
+    authRequest(username: ApplicationSettings.username!, password: ApplicationSettings.password!, url: ApplicationSettings.favoriteAPI, completionHandler: {response, data in
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            do {
+                let favorites = try decoder.decode(FavoriteResponse.self, from: data)
+                completionHandler(favorites.results)
+            } catch let error {
+                print(error)
+            }
+        }
+    })
 }
 
 func getHistory(completionHandler: @escaping ([Pomodoro]) -> Void) {
-    let url = URL.init(string: ApplicationSettings.pomodoroAPI)
-    var request = URLRequest.init(url: url!)
-
-    let loginString = "\(ApplicationSettings.username!):\(ApplicationSettings.password!)"
-
-    guard let loginData = loginString.data(using: String.Encoding.utf8) else {
-        return
-    }
-    let base64LoginString = loginData.base64EncodedString()
-
-    request.httpMethod = "GET"
-    request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("application/json", forHTTPHeaderField: "Accept")
-
-    let task = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error -> Void in
-        if let httpResponse = response as? HTTPURLResponse {
-            print(httpResponse)
+    authRequest(username: ApplicationSettings.username!, password: ApplicationSettings.password!, url: ApplicationSettings.pomodoroAPI, completionHandler: {response, data in
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
             do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                do {
-                    let pomodoros = try decoder.decode(PomodoroResponse.self, from: data!)
-                    completionHandler(pomodoros.results)
-                } catch let error {
-                    print(error)
-                }
-            }}
+                let pomodoros = try decoder.decode(PomodoroResponse.self, from: data)
+                completionHandler(pomodoros.results)
+            } catch let error {
+                print(error)
+            }
+        }
     })
-
-    task.resume()
 }
-

f8b6bb1203d66883c084b928e7abaa3f80873ed4
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-08-22 10:08:14 +0900

    Initial TodayView

--- NextPomodoroToday/TodayViewController.swift
+++ NextPomodoroToday/TodayViewController.swift
@@ -9,16 +9,84 @@
 import UIKit
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
+class TodayViewController: UITableViewController, NCWidgetProviding {
+    var data: Pomodoro?
+    var timer = Timer()
+
+    @IBOutlet weak var titleCell: UITableViewCell!
+    @IBOutlet weak var categoryCell: UITableViewCell!
+    @IBOutlet weak var countdownCell: UITableViewCell!
+
+    // MARK: - custom
+
+    @objc func refreshData() {
+        if ApplicationSettings.username != nil {
+            getHistory(completionHandler: { favorites in
+                self.data = favorites.sorted(by: { $0.id > $1.id })[0]
+                self.updateCounter()
+                self.updateView()
+            })
+        }
+    }
+
+    @objc func updateCounter() {
+        if let data = data {
+            let formatter = DateComponentsFormatter()
+            formatter.allowedUnits = [.hour, .minute, .second]
+            formatter.unitsStyle = .positional
+            formatter.zeroFormattingBehavior = .pad
+
+            var elapsed = Date().timeIntervalSince(data.end)
+
+            if elapsed > 0 {
+                setCountdown(color: elapsed > 300 ? UIColor(named: "LateTimer")! : UIColor(named: "BreakTimer")!, text: formatter.string(from: TimeInterval(elapsed))!)
+            } else {
+                elapsed *= -1
+                setCountdown(color: UIColor.init(named: "ActiveTimer")!, text: formatter.string(from: TimeInterval(elapsed))!)
+            }
+        }
+    }
+
+    func updateView() {
+        DispatchQueue.main.async {
+            if let data = self.data {
+                self.titleCell.detailTextLabel?.text = data.title
+                self.categoryCell.detailTextLabel?.text = data.category
+            }
+        }
+    }
+
+    func setCountdown(color: UIColor, text: String) {
+        DispatchQueue.main.async {
+            self.countdownCell.detailTextLabel?.text = text
+            self.countdownCell.backgroundColor = color
+        }
+    }
+
+    // MARK: - lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        refreshData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(updateCounter),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
     }
 
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -27,8 +95,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-
+        refreshData()
         completionHandler(NCUpdateResult.newData)
     }
-
 }
